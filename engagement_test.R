78b23150a249642d944330d059cd970edcefa6ee
ivypink1 <ivypink1@gmail.com>
2018-04-14 00:08:56 -0700

    Add files via upload

---
+++ engagement_test.R
@@ -0,0 +1,10 @@
+#engagement test
+
+setwd('/Users/sma76/Desktop/data science/R/take-home challenges/engagement/Engagement_Test')
+
+t<-read.csv('test_table.csv', header = TRUE)
+u<-read.csv('user_table.csv', header = TRUE)
+
+
+
+
